96fcd446b2863147d1ee729e9931603f27e2c3d4
Ben Blackmore <ben.blackmore@steadybit.com>
2022-07-14 15:01:16 +0200

    feat: set a default duration

--- request_termination.go
+++ request_termination.go
@@ -25,20 +25,21 @@ type RequestTerminationState struct {
 func describeRequestTermination(w http.ResponseWriter, _ *http.Request, _ []byte) {
 	writeBody(w, DescribeAttackResponse{
 		Id:          "com.github.steadybit.extension_kong.request_termination",
-		Label:       "Terminate requests in Kong",
+		Label:       "Terminate requests",
 		Description: "Leverage the Kong request-termination plugin to inject HTTP failures.",
-		Version:     "1.0.1",
+		Version:     "1.1.0",
 		Icon:        "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='64' height='64'%3E%3Cpath d='M20.986 50.552h11.662l6.055 7.54-1.04 2.568H22.596l.37-2.568-3.552-5.548zm8.238-33.765 6.33-.01L64 50.428l-2.2 10.23H49.61l.76-2.883-26.58-31.452zM40.518 3.34 53.68 13.758l-1.685 1.75 2.282 3.2v3.422l-6.563 5.386L36.68 14.39h-6.426l2.587-4.774zm-27.46 32.852 9.256-7.935L34.6 42.84l-3.5 5.342H19.782l-7.837 10.144-1.8 2.333H0V48.213l9.465-12.02z' fill='%23003459' fill-rule='evenodd'/%3E%3C/svg%3E",
 		Target:      "com.github.steadybit.extension_kong.service",
 		Category:    "network",
 		TimeControl: "EXTERNAL",
 		Parameters: []AttackParameter{
 			{
-				Label:    "Duration",
-				Name:     "duration",
-				Type:     "duration",
-				Advanced: false,
-				Required: true,
+				Label:        "Duration",
+				Name:         "duration",
+				Type:         "duration",
+				Advanced:     false,
+				Required:     true,
+				DefaultValue: "30s",
 			},
 			{
 				Label:       "Consumer Username or ID",
