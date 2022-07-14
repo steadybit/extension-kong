783d0c8e45a7c345aa2e80f45b758a1b47da4408
Ben Blackmore <ben.blackmore@steadybit.com>
2022-07-14 10:12:32 +0200

    chore: apply regular English casing rules

--- services.go
+++ services.go
@@ -13,9 +13,9 @@ import (
 func describeServiceType(w http.ResponseWriter, _ *http.Request, _ []byte) {
 	writeBody(w, DescribeTargetTypeResponse{
 		Id:       "com.github.steadybit.extension_kong.service",
-		Label:    PluralLabel{One: "Kong Service", Other: "Kong Services"},
+		Label:    PluralLabel{One: "Kong service", Other: "Kong services"},
 		Category: "API gateway",
-		Version:  "1.1.0",
+		Version:  "1.1.1",
 		Icon:     "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='64' height='64'%3E%3Cpath d='M20.986 50.552h11.662l6.055 7.54-1.04 2.568H22.596l.37-2.568-3.552-5.548zm8.238-33.765 6.33-.01L64 50.428l-2.2 10.23H49.61l.76-2.883-26.58-31.452zM40.518 3.34 53.68 13.758l-1.685 1.75 2.282 3.2v3.422l-6.563 5.386L36.68 14.39h-6.426l2.587-4.774zm-27.46 32.852 9.256-7.935L34.6 42.84l-3.5 5.342H19.782l-7.837 10.144-1.8 2.333H0V48.213l9.465-12.02z' fill='%23003459' fill-rule='evenodd'/%3E%3C/svg%3E",
 		Table: Table{
 			Columns: []Column{
@@ -41,39 +41,39 @@ func describeKongTypeAttributes(w http.ResponseWriter, _ *http.Request, _ []byte
 			{
 				Attribute: "kong.instance.name",
 				Label: PluralLabel{
-					One:   "Kong Instance Name",
-					Other: "Kong Instance Names",
+					One:   "Kong instance name",
+					Other: "Kong instance names",
 				},
 			}, {
 				Attribute: "kong.service.name",
 				Label: PluralLabel{
-					One:   "Kong Service Name",
-					Other: "Kong Service Names",
+					One:   "Kong service name",
+					Other: "Kong service names",
 				},
 			}, {
 				Attribute: "kong.service.id",
 				Label: PluralLabel{
-					One:   "Kong Service Id",
-					Other: "Kong Service Ids",
+					One:   "Kong service ID",
+					Other: "Kong service IDs",
 				},
 			},
 			{
 				Attribute: "kong.service.url",
 				Label: PluralLabel{
-					One:   "Kong Service URL",
-					Other: "Kong Service URLs",
+					One:   "Kong service URL",
+					Other: "Kong service URLs",
 				},
 			}, {
 				Attribute: "kong.service.tag",
 				Label: PluralLabel{
-					One:   "Kong Service Tag",
-					Other: "Kong Service Tags",
+					One:   "Kong service tag",
+					Other: "Kong service tags",
 				},
 			}, {
 				Attribute: "kong.service.enabled",
 				Label: PluralLabel{
-					One:   "Kong Service Enabled",
-					Other: "Kong Service Enabled",
+					One:   "Kong service enabled",
+					Other: "Kong service enabled",
 				},
 			},
 		},
